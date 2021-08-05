f6d8da7f12660a59fcf13cf78dca26894b600d73
Crowdbotics <team@crowdbotics.com>
2021-08-05 01:49:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "super_water_29490Identifier",
+  url: "https://super_water_29490Identifier.botics.co",
+  api: "https://super_water_29490Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
